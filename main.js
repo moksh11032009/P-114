96f02c114b0996a779ffe0e81964de1e325b375c
moksh11032009 <120463218+moksh11032009@users.noreply.github.com>
2023-01-08 21:07:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload() {
+}
+
+function setup() {
+    create = createCanvas(300,300);
+    canvas.center(); 
+}
+
+function draw() { 
+
+}
+
+function take_snapshot() {
+    save("my_picture.png");
+}
